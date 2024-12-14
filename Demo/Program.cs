c4f0061620d73d466cf81ec0ae6466e256943f36
Hassan <hassansayedelkholy@gmail.com>
2024-12-14 14:30:59 +0200

    Function Prototype and Calling

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,34 @@
 {
     class Program
     {
+        #region Function Prototype and Calling
+        //static void PrintShapes()
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        Console.WriteLine("*_*");
+        //    }
+
+        //}
+        //static void PrintShapes(int count, string shape)
+        //{
+        //    for (int i = 1; i <= count; i++)
+        //    {
+        //        Console.WriteLine($"{shape}");
+        //    }
+
+        //}
+        #endregion
         static void Main(string[] args)
         {
-           
+            #region Function Prototype and Calling
+            // PrintShapes();   //Calling 
+            //PrintShapes(count: 10, shape:"#_#");   //calling by name
+            //PrintShapes(10, "#_#");                //caliing by order
+            //string folderPath = @"E:\Route";
+            #endregion
+
+
         }
     }
 }
