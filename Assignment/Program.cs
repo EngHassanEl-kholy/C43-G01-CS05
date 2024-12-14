2f60c3b95925c8cc85e17def6f795df5e62e4b5e
Hassan <hassansayedelkholy@gmail.com>
2024-12-14 18:35:32 +0200

    Q01 [Value Type Parameters]

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -2,9 +2,29 @@
 {
     class Program
     {
+        #region Q01
+        //static void IncrementNumber(int number)
+        //{
+        //    number += 10;
+        //}
+        //static void IncrementNumber(ref int number)
+        //{
+        //    number += 20;
+        //}
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            //1-Explain the difference between passing (Value type parameters)
+            //by value and by reference then write a suitable c# example.
+            // int Num = 10;
+            //IncrementNumber(Num);     //Pass By Value 
+            //Console.WriteLine(Num);   // will print 10 because it passed by value
+            //IncrementNumber(ref Num);   //pass by reference
+            //Console.WriteLine(Num);     // will print 30
+            #endregion
+
+
         }
     }
 }
